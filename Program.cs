cf0f3c1053c68647952ec2ae5bfa77ea8d576053
Radioactivity <85001747+voidwyrm-2@users.noreply.github.com>
2025-05-03 17:36:30 -0400

    Removed needlessly long line

--- Program.cs
+++ Program.cs
@@ -7,12 +7,12 @@ namespace Todd;
 
 public static class Program
 {
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+#pragma warning disable CS8618
     // ReSharper disable once MemberCanBePrivate.Global
     internal static DiscordRpcClient client;
     // ReSharper disable once MemberCanBePrivate.Global
     internal static ToddLogger logger;
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+#pragma warning restore CS8618
 
     private const string TODD_FOLDER = ".todd";
     
